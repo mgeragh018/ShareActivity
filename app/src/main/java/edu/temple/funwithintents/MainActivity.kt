b3eee9af11b3c5f73d8f1d0596e620c736a9f88d
Michael Geraghty <tup13771@temple.edu>
2024-11-07 14:42:19 -0500

    finished

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -13,9 +14,21 @@ class MainActivity : AppCompatActivity() {
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
 
+
+
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            val textToShare = editText.text.toString()
+            if (textToShare.isNotEmpty()) {
+                val sendIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, textToShare)
+                    type = "text/plain"
+                }
+                if (sendIntent.resolveActivity(packageManager) != null) {
+                    startActivity(sendIntent)
+                }
+            }
         }
     }
 }
